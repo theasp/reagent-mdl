07d9a2b4348a0b2d3577304591cb028df5c14516
Andrew Phillips <theasp@gmail.com>
2017-01-21 09:31:22 -0400

    Fix test

---
+++ test/ca/gt0/theasp/reagent_mdl_test.clj
@@ -0,0 +1,3 @@
+(ns ca.gt0.theasp.reagent-mdl-test
+  (:require [clojure.test :refer :all]
+            [ca.gt0.theasp.reagent-mdl :refer :all]))
--- test/io/github/theasp/reagent_mdl_test.clj
+++
@@ -1,7 +0,0 @@
-(ns io.github.theasp.reagent-mdl-test
-  (:require [clojure.test :refer :all]
-            [io.github.theasp.reagent-mdl :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
