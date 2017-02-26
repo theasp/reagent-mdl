4bbec128df8e688fc1357c7ab3a04af3b4174a64
Andrew Phillips <theasp@gmail.com>
2017-02-26 11:48:13 -0400

    Even newer cljs

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.473"]
+                 [org.clojure/clojurescript "1.9.494"]
                  [cljsjs/material "1.3.0-0"]
                  [cljsjs/dialog-polyfill "0.4.3-0"]
                  [com.taoensso/timbre "4.8.0"]
